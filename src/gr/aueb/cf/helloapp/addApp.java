43cd394cf6f9746156477c0c8d7f550940d4b002
baxnse <baxn.se@gmail.com>
2025-07-24 19:11:05 +0300

    fix: added missing sout

--- src/gr/aueb/cf/helloapp/addApp.java
+++ src/gr/aueb/cf/helloapp/addApp.java
@@ -5,6 +5,7 @@ public class addApp {
     public static void main(String[] args) {
         System.out.println(add(3,5));
         System.out.printf("%d", add(3,5));
+        System.out.println("test");
     }
 
     public static int add(int a, int b) {
