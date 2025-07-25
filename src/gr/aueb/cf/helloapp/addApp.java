07645f40b899217ea0adfa7ccc0aa163d990c8d5
baxnse <baxn.se@gmail.com>
2025-07-25 11:00:29 +0300

    again add pol method, added pol print

--- src/gr/aueb/cf/helloapp/addApp.java
+++ src/gr/aueb/cf/helloapp/addApp.java
@@ -7,10 +7,15 @@ public class addApp {
         System.out.printf("%4d", add(7,2));
         System.out.printf("%d\n", add(3,5));
         //System.out.println("test");
+        System.out.printf("result: %d", pol(10, 20));
     }
 
     public static int add(int a, int b) {
         return a + b;
+    }
+
+    public static int pol(int a, int b) {
+        return a*b;
     }
 
     public static int sub (int a, int b) {
