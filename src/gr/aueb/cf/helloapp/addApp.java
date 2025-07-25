f44b29da0328947f9d0f6ddb0016e4c6cff608af
baxnse <baxn.se@gmail.com>
2025-07-25 10:16:27 +0300

    add new method, add new print

--- src/gr/aueb/cf/helloapp/addApp.java
+++ src/gr/aueb/cf/helloapp/addApp.java
@@ -7,6 +7,7 @@ public class addApp {
         System.out.printf("%4d", add(7,2));
         System.out.printf("%d\n", add(3,5));
         System.out.println("test");
+        System.out.printf("result: %d", pol(10, 20));
     }
 
     public static int add(int a, int b) {
@@ -15,5 +16,9 @@ public class addApp {
 
     public static int sub (int a, int b) {
         return a - b;
+    }
+
+    public static int pol(int a, int b) {
+        return a*b;
     }
 }
